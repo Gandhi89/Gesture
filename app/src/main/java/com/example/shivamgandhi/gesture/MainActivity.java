272f46086ed47c04e9a1be3fca989d8c3245276c
shivamGandhi <shivamgandhi95@gmail.com>
2018-08-17 22:08:21 -0400

    Implemented SignIn flow in the app

--- app/src/main/java/com/example/shivamgandhi/gesture/MainActivity.java
+++ app/src/main/java/com/example/shivamgandhi/gesture/MainActivity.java
@@ -2,27 +2,78 @@ package com.example.shivamgandhi.gesture;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
+
+import com.firebase.ui.auth.AuthUI;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+import java.util.Arrays;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     Button createGameBtn, joinGameBtn;
     GameDatabase mGameDatabase;
     Vars mVars;
+    private FirebaseAuth mFirebaseAuth;
+    private FirebaseAuth.AuthStateListener mAuthStateListener;
+    public static final int RC_SIGN_IN = 1;
+    String mUserName;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        mAuthStateListener = new FirebaseAuth.AuthStateListener() {
+
+            @Override
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+
+
+                FirebaseUser user = firebaseAuth.getCurrentUser();
+                if (user != null){
+                    Log.d("MainActivity/log","5");
+                    // user is signed in
+                    onSignInInitiaize(user.getDisplayName());
+                    Toast.makeText(MainActivity.this, "You are now sign in.Welcome!", Toast.LENGTH_SHORT).show();
+                }
+                else {
+                    Log.d("MainActivity/log","6");
+
+                    // user is signed out
+                    onSignOutCleanUp();
+                    startActivityForResult(
+                            AuthUI.getInstance()
+                                    .createSignInIntentBuilder()
+                                    .setIsSmartLockEnabled(false)
+                                    .setAvailableProviders(Arrays.asList(
+                                            new AuthUI.IdpConfig.GoogleBuilder().build(),
+                                            new AuthUI.IdpConfig.EmailBuilder().build()))
+                                    .build(),
+                            RC_SIGN_IN);
+                }
+
+            }
+        };
+        Log.d("MainActivity/log","0");
         if (getIntent().getBooleanExtra("EXIT", false)) {
+            Log.d("MainActivity/log","1");
+
             finish();
             return;
         }
+        Log.d("MainActivity/log","2");
+
         setContentView(R.layout.activity_main);
         mGameDatabase = new GameDatabase();
         mVars = Vars.getInstance();
+
+        mFirebaseAuth = FirebaseAuth.getInstance();
 
         createGameBtn = findViewById(R.id.mainActivity_creategame);
         joinGameBtn = findViewById(R.id.mainActivity_joingame);
@@ -30,7 +81,27 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
         createGameBtn.setOnClickListener(this);
         joinGameBtn.setOnClickListener(this);
+        Log.d("MainActivity/log","3");
 
+        /**
+         * check if user is signed in or not
+         */
+
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == RC_SIGN_IN){
+            if(resultCode == RESULT_OK){
+                Toast.makeText(this, "Signed in!", Toast.LENGTH_SHORT).show();
+            }
+            else if (resultCode == RESULT_CANCELED){
+                Toast.makeText(this, "Sign in canceled", Toast.LENGTH_SHORT).show();
+                finish();
+            }
+        }
 
     }
 
@@ -61,5 +132,31 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                 startActivity(i);
                 break;
         }
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.d("MainActivity/log","onResume");
+        mFirebaseAuth.addAuthStateListener(mAuthStateListener);
+
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.d("MainActivity/log","onPause");
+        mFirebaseAuth.removeAuthStateListener(mAuthStateListener);
+    }
+
+    private void onSignInInitiaize(String userName){
+
+        mUserName = userName;
+
+    }
+    private void onSignOutCleanUp(){
+
+        mUserName = "";
+
     }
 }
